8ff5ca935f6c501bc4f7fb79e1b2fd7a26a8a1ed
Manoj Kumar <mano.mykingdom@gmail.com>
2015-05-25 00:16:55 +0530

    updated readme and example

--- example/app.js
+++ example/app.js
@@ -1,34 +1,37 @@
+var DownloaderModule = require("com.mykingdom.downloader");
 var win = Ti.UI.createWindow({
-	backgroundColor: "#fff",
-	exitOnClose: true
+	backgroundColor : "#fff",
+	exitOnClose : true
 });
 var downloadBtn = Ti.UI.createButton({
-	title: "Start Download"
+	title : "Start Download"
 });
-downloadBtn.addEventListener("click", function(e){
-	var DownloaderModule = require("com.mykingdom.downloader");
+downloadBtn.addEventListener("click", function(e) {
 	var downloader = DownloaderModule.createAsyncDownloader({
-		filesToDownload: [{
-			url : "https://registry.npmjs.org/alloy/-/alloy-1.4.0-rc.tgz",
-			name: "alloy.tgz"
-		}, {
-			url : "http://builds.appcelerator.com/mobile/3.3.0/mobilesdk-3.3.0.RC-osx.zip"
+		filesToDownload : [{
+			url : "https://ebinders.etcconnect.com/assets/uploads/files/en-us/63a26-White_Paper_Control_Philosophy_revA.pdf",
+			name : "White_Paper_Control_Philosophy_revA1.pdf"
 		}],
-		outputDirectory: Ti.Filesystem.getFile(Ti.Filesystem.getExternalStorageDirectory()),
-		enableNotification: true,
-		notificationId: 1,
-		notificationTitle: "Downloading Appcelerator SDK"
+		outputDirectory : Ti.Filesystem.getFile(Ti.Filesystem.getExternalStorageDirectory()),
+		useCache : false,
+		enableNotification : true,
+		notificationId : 1,
+		notificationTitle : "Custom Title",
+		notificationSuccessTitle : "Custom Success Title",
+		notificationSuccessDescription : "Custom Success Description",
+		notificationFailureTitle : "Custom Failure Title",
+		notificationFailureDescription : "Custom Failure Description"
 	});
-	downloader.addEventListener("error", function(evt){
+	downloader.addEventListener("error", function(evt) {
 		alert(evt.error + " : While downloading file at (index 0 based) = " + evt.currentIndex);
 	});
-	downloader.addEventListener("onload", function(evt){
+	downloader.addEventListener("onload", function(evt) {
 		Ti.API.info("Current Progress = " + evt.progress);
 	});
-	downloader.addEventListener("cancel", function(evt){
+	downloader.addEventListener("cancel", function(evt) {
 		Ti.API.info("Download canceled");
 	});
-	downloader.addEventListener("success", function(evt){
+	downloader.addEventListener("success", function(evt) {
 		Ti.API.info("Download completed successfully");
 	});
 	downloader.startDownload();
